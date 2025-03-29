df4774e01f2bb8bff222d74f22c187507f26bd0b
Eskndr <eskndrytbarek323@gmail.com>
2025-03-29 22:47:50 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,42 @@ import Login from "./pages/Login";
 import Singnup from "./pages/Singnup";
 import VerifyEmail from "./pages/VerifyEmail";
 
+const isLoggedIn = localStorage.getItem("token");
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route
+          path="/"
+          element={!isLoggedIn ? <Login /> : <Navigate to="/home" />}
+        />
+        <Route
+          path="/home"
+          element={isLoggedIn ? <Home /> : <Navigate to="/" />}
+        />
         <Route path="/signup" element={<Singnup />} />
         <Route path="/verify-email" element={<VerifyEmail />} />
-        <Route path="/books/create" element={<CreateBook />} />
-        <Route path="/books/details/:id" element={<ShowBook />} />
-        <Route path="/books/edit/:id" element={<EditBook />} />
-        <Route path="/books/delete/:id" element={<DeleteBook />} />
+        <Route
+          path="/books/create"
+          element={isLoggedIn ? <CreateBook /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/books/details/:id"
+          element={isLoggedIn ? <ShowBook /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/books/edit/:id"
+          element={isLoggedIn ? <EditBook /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/books/delete/:id"
+          element={isLoggedIn ? <DeleteBook /> : <Navigate to="/" />}
+        />
       </Routes>
     </BrowserRouter>
   );
 }
+
 
 export default App;
