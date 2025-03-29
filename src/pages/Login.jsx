2672e866556f90207ca38a947f6ffdd572322ccb
Eskndr <eskndrytbarek323@gmail.com>
2025-03-29 23:37:10 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useSnackbar } from "notistack";
 import axiosInstance from "../services";
 
@@ -9,6 +9,7 @@ const Login = () => {
 
 
   const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
 
 
 
